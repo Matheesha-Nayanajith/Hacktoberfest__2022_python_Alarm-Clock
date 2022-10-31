106cfabaf912dc41f401a06e6de7041c21ef2f56
Rajitha Fernando <35393733+RajithaFernando@users.noreply.github.com>
2022-10-31 23:23:00 +0530

    Create ABS.py

---
+++ ABS.py
@@ -0,0 +1,19 @@
+number = -20
+
+absolute_number = abs(number)
+print(absolute_number)
+
+# Output: 20
+
+# The abs() function returns the absolute value of the given number. If the number is a complex number, abs() returns its magnitude.
+
+# Example
+
+
+# random integer
+integer = -20
+print('Absolute value of -20 is:', abs(integer))
+
+#random floating number
+floating = -30.33
+print('Absolute value of -30.33 is:', abs(floating))
